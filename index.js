0852b4ec7b995f8c73ecb90abc03cc4945f7528e
Sunil Chauhan <sunil121ml@gmail.com>
2023-03-29 16:28:30 +0530

    adding movie dropdown

--- index.js
+++ index.js
@@ -5,13 +5,57 @@ const fetchData = async (searchInput) => {
       s: searchInput,
     },
   });
-  console.log(res.data);
+  if (res.data.Error) {
+    return [];
+  }
+  return res.data.Search;
 };
 
+const root = document.querySelector(".autoComplete");
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input is-primary" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
+
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+
+  if (input.value === "") dropdown.classList.remove("is-active");
+
+  if (!movies.length) {
+    const p = document.createElement("p");
+    p.innerText = "No Movies Found";
+    resultsWrapper.appendChild(p);
+  } else {
+    movies.forEach((movie) => {
+      const item = document.createElement("a");
+      const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+      item.classList.add("dropdown-item");
+      item.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title} (${movie.Year})
+      `;
+      resultsWrapper.appendChild(item);
+    });
+  }
 };
 
 input.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener("click", (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
